5824429375700d5eeae9a02e18b2100fbc45a6ac
مُحَمَّد <mhmdnoman>
2023-07-21 23:41:49 +0600

    adde container

--- Milestone 3 - CSS Frameworks/penguin-fashion-with-tailwind/tailwind.config.js
+++ Milestone 3 - CSS Frameworks/penguin-fashion-with-tailwind/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'bebas-neue': ['"Bebas Neue"', 'sans-serif'],
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
+
+// 
+// 
+// 
