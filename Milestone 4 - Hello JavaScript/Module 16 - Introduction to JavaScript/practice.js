a90223229985bbf506bda280bc0e3b91ae04e1c6
مُحَمَّد <mhmdnoman>
2023-07-29 12:47:43 +0600

    new milestone

---
+++ Milestone 4 - Hello JavaScript/Module 16 - Introduction to JavaScript/practice.js
@@ -0,0 +1,30 @@
+// Practice Problem 1
+const totalMoney = 1000;
+const oneKiloApple = 150;
+const oneKiloOrange = 150;
+const moneyLeft = totalMoney - (oneKiloApple + oneKiloOrange);
+
+console.log(moneyLeft);
+
+// Practice Problem 2
+const math = 75.25;
+const bio = 65;
+const chem = 80;
+const phy = 35.45;
+const bang = 99.50;
+const totalMarks = math + bio + chem + phy + bang;
+const average = totalMarks / 5;
+
+console.log(average.toFixed(2));
+
+// Practice Problem 3
+const str1 = 'I am going to be'
+const str2 = "an awesome web developer"
+const strCon = str1 + " " + str2;
+
+console.log(strCon);
+
+// Practice Problem 4
+const numb = 119;
+const remindr = 119 % 5;
+console.log(remindr);
